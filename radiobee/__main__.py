1bdc1b2817662978e3f87ee670d6b720560e9cf9
freemt <yucongo+fmt@gmail.com>
2021-12-25 16:33:23 +0800

    Update

--- radiobee/__main__.py
+++ radiobee/__main__.py
@@ -384,7 +384,7 @@ if __name__ == "__main__":
         *   `tf_type` `itf_type` `dl_type` `norm`: Normally there is no need to touch these unless you know what you are doing.
         *   Suggested `min_samples` and `esp` values -- `min_samples`: 4-8, `esp` (minimum epsilon): 8-12.
            -   Smaller `min_samples` or larger `esp` will result in more aligned pairs but also more **false positives** (pairs falsely identified as candidates). On the other hand, larger `min_samples` or smaller `esp` values tend to miss 'good' pairs.
-        *   If you need a better image, you can right-click and select copy-image-address and open a new tab in the browser with the copied image address.
+        *   If you need to have a better look at the image, you can right-click on the image and select copy-image-address and open a new tab in the browser with the copied image address.
     """
     )
     css = ".output_image, .input_image {height: 40rem !important; width: 100% !important;}"
